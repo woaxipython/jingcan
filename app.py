b854bf8d7546dfaaff68cdba8b5b8a602277e746
qhzzeg <qhzzrg@gamil.com>
2023-06-04 13:33:35 +0800

    test 04

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from models.store import ParentOrderModel
 app = Flask(__name__)
 
 # config初始化
-app.config.from_object(config.TestingConfig)
+app.config.from_object(config.DevelopmentConfig)
 
 # SQlAlchemy初始化
 db.init_app(app=app)
